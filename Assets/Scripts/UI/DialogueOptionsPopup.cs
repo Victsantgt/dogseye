cc40e53e445d99ffb44d2aaae5e6fa2f4a495a7d
MrSae5 <mdemiguel2017@gmail.com>
2026-09-05 03:28:28 +0200

    Textos de elecciones

---
+++ Assets/Scripts/UI/DialogueOptionsPopup.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DialogueOptionsPopup : MonoBehaviour
+{
+    [SerializeField] private ImageOption[] options;
+
+    public void ShowOptions(int index)
+    {
+        if (options == null) return;
+
+        foreach (ImageOption option in options)
+        {
+            if (option == null || option.rectTransform == null)
+                continue;
+
+            if (option.textMesh != null && option.texts != null &&
+                index >= 0 && index < option.texts.Length)
+            {
+                option.textMesh.text = option.texts[index];
+            }
+
+            if (!option.rectTransform.gameObject.activeSelf)
+                option.rectTransform.gameObject.SetActive(true);
+
+            PopOut.pop(option.rectTransform);
+        }
+    }
+}
