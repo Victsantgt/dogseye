ffa61536f0f5c0f798d718c1fb7a20073f524935
Victor Santiago Gil Torres <victsantgt@gmail.com>
2026-09-06 03:51:49 +0200

    menú muerte y sonido fallo

--- Assets/Scripts/Patrones/Observer/Observer/LifeManager.cs
+++ Assets/Scripts/Patrones/Observer/Observer/LifeManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class LifeManager : MonoBehaviour, IObserver<NoteHitInfo>
 {
-    //public Transitions transicion;
+    public Transitions transicion;
 
     // Secuencia de muerte del jugador. La referencia esta viva y ya asignada en la
     // escena; lo unico comentado es la llamada de abajo, en UpdateObserver().
@@ -49,15 +49,15 @@ public class LifeManager : MonoBehaviour, IObserver<NoteHitInfo>
                 break;
             case HitResult.Good:
                 currentLife += lifeGainGood;
-                //MusicManager.Instance.Play_SFX(noteAudio[1]);
+                //MusicManager.Instance.Play_SFX(noteAudio[0]);
                 break;
             case HitResult.Bad:
                 currentLife -= lifeLoseBad;
-                //MusicManager.Instance.Play_SFX(noteAudio[2]);
+                MusicManager.Instance.Play_SFX(noteAudio[0]);
                 break;
             case HitResult.Miss:
                 currentLife -= lifeLoseMiss;
-                //MusicManager.Instance.Play2D_SFX(noteAudio[3]);
+                MusicManager.Instance.Play_SFX(noteAudio[0]);
                 break;
         }
 
@@ -65,13 +65,7 @@ public class LifeManager : MonoBehaviour, IObserver<NoteHitInfo>
         currentLife = Mathf.Clamp(currentLife, 0f, maxLife);
         if (currentLife <= 0f)
         {
-            //transicion.LoseTransition();
-
-            // DESCOMENTA ESTA LINEA cuando el sistema de notas este arreglado y la vida
-            // sea fiable. Dispara la misma secuencia de muerte que ahora se prueba a mano
-            // con la tecla M (frenado, notas fuera, fundido a negro y R para reiniciar).
-            // Al descomentarla conviene poner UsarTeclaDePrueba a false en el jugador.
-            //if (muerte != null) muerte.Morir();
+            transicion.LoseTransition();
         }
 
     }
